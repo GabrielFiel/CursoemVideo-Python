d7c55c69f9d4e86cad7d2cec08efdd4d9340d17d
Gabriel Fiel <12551784+GabrielFiel@users.noreply.github.com>
2018-06-22 19:10:04 -0300

    Exercicio 23

---
+++ ex023.py
@@ -0,0 +1,11 @@
+num = input('Digite um número de 0 a 9999')
+
+milhar = num[0]
+centena = num[1]
+dezena = num[2]
+unidade = num[3]
+
+print('Milhar = {}'.format(milhar))
+print('Centena = {}'.format(centena))
+print('Dezena = {}'.format(dezena))
+print('Unidade = {}'.format(unidade))
